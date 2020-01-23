24cc354eead734bef9012ffb8264037f269c211c
Ricardo Serrano <rxsh964@gmail.com>
2020-01-23 03:31:04 -0500

    image controller modifications

--- conversion_services/MyImageConverterWeb/src/main/java/MyImageConverterWeb/ImageContoller.java
+++ conversion_services/MyImageConverterWeb/src/main/java/MyImageConverterWeb/ImageContoller.java
@@ -5,12 +5,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javacv.ImageConverter;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 
@@ -35,7 +38,7 @@ public class ImageContoller  {
 	 * @throws IOException
 	 */
 	@PostMapping("/pngimage")
-	public ResponseEntity<Resource> convert(@RequestParam("image") MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
+	public ResponseEntity<Resource> pngFile(@RequestParam("png") MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
 		
 		String convertedFilePath = new ImageConverter().pngConversion(multipartFile);
 		
@@ -51,40 +54,35 @@ public class ImageContoller  {
 	        return ResponseEntity.ok()
 		    		.headers(header)
 		            .contentLength(convertedFile.length())
-		            .contentType(MediaType.parseMediaType("application/octet-stream"))
+		            .contentType(MediaType.parseMediaType("image/png"))
+		            .body(resource);
+		}
+		return null;
+	}
+
+	
+	@PostMapping("/jpgimage")
+	public ResponseEntity<Resource> jpgFile(@RequestParam("jpg") MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
+		
+		String convertedFilePath = new ImageConverter().jpgConversion(multipartFile);
+		
+		if(convertedFilePath != null) {
+			File convertedFile = new File(convertedFilePath);
+			InputStreamResource resource = new InputStreamResource(new FileInputStream(convertedFile));
+			HttpHeaders header = new HttpHeaders();
+	        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+	        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+	        header.add("Pragma", "no-cache");
+	        header.add("Expires", "0");
+	        
+	        return ResponseEntity.ok()
+		    		.headers(header)
+		            .contentLength(convertedFile.length())
+		            .contentType(MediaType.parseMediaType("image/jpeg"))
 		            .body(resource);
 		}
 		return null;
 	}
 	
-	
-	@PostMapping("/imagepng")
-	public Image pngFile(@RequestParam("png") MultipartFile file, RedirectAttributes redirectAttributes) {
-		BufferedImage bimage = new ImageConverter().convertToPNG(file);
-		Image image = (Image)bimage;
-		return image;
-	}
-	
-	@PostMapping("/imagejpg")
-	public Image jpgFile(@RequestParam("jpg") MultipartFile file, RedirectAttributes redirectAttributes) {
-		BufferedImage bimage = new ImageConverter().convertToJPG(file);
-		Image image = (Image)bimage;
-		return image;
-	}
-	
-	@PostMapping("/imagebmp")
-	public Image bmpFile(@RequestParam("bmp") MultipartFile file, RedirectAttributes redirectAttributes) {
-		BufferedImage bimage = new ImageConverter().convertToBMP(file);
-		Image image = (Image)bimage;
-		return image;
-	}
-	
-	@PostMapping("/imagetiff")
-	public Image tifFile(@RequestParam("tiff") MultipartFile file, RedirectAttributes redirectAttributes) {
-		BufferedImage bimage = new ImageConverter().convertToTIFF(file);
-		Image image = (Image)bimage;
-		return image;
-	}
-
 	
 }
--- conversion_services/MyImageConverterWeb/src/main/java/javacv/ImageConverter.java
+++ conversion_services/MyImageConverterWeb/src/main/java/javacv/ImageConverter.java
@@ -37,80 +37,21 @@ public class ImageConverter {
 	}
 	
 	
-	public BufferedImage convertToPNG(MultipartFile file) {
-		File myFile = FileConverter.convertMultiPartToFile(file);
+	/**
+	 * Method that converts an image to JPG format
+	 * @param multipartFile from post request data
+	 * @return the new converted image path
+	 */
+	public String jpgConversion(MultipartFile multipartFile) {
+		File myFile = FileConverter.convertMultiPartToFile(multipartFile);
 		Mat image = Imgcodecs.imread(myFile.getPath());
-		
-		MatOfByte matOfByte = new MatOfByte();
-		Imgcodecs.imencode(".png", image, matOfByte);
-		
-		Imgcodecs.imwrite(myFile.getAbsolutePath()+".png", image);
-		
+		String convertedFilePath = myFile.getPath()+".jpg";
 		FileConverter.deleteFile(myFile);
-		try {
-			return ImageIO.read(new ByteArrayInputStream(matOfByte.toArray()));
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(Imgcodecs.imwrite(convertedFilePath, image)) {
+			return convertedFilePath;
 		}
 		return null;
 	}
 	
-	public BufferedImage convertToJPG(MultipartFile file) {
-		File myFile = FileConverter.convertMultiPartToFile(file);
-		Mat image = Imgcodecs.imread(myFile.getPath());
-		
-		MatOfByte matOfByte = new MatOfByte();
-		Imgcodecs.imencode(".jpg", image, matOfByte);
-		
-		Imgcodecs.imwrite(myFile.getAbsolutePath()+".jpg", image);
-		
-		FileConverter.deleteFile(myFile);
-		try {
-			return ImageIO.read(new ByteArrayInputStream(matOfByte.toArray()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public BufferedImage convertToBMP(MultipartFile file) {
-		File myFile = FileConverter.convertMultiPartToFile(file);
-		Mat image = Imgcodecs.imread(myFile.getPath());
-		
-		MatOfByte matOfByte = new MatOfByte();
-		Imgcodecs.imencode(".bmp", image, matOfByte);
-		
-		Imgcodecs.imwrite(myFile.getAbsolutePath()+".bmp", image);
-		
-		FileConverter.deleteFile(myFile);
-		try {
-			return ImageIO.read(new ByteArrayInputStream(matOfByte.toArray()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public BufferedImage convertToTIFF(MultipartFile file) {
-		File myFile = FileConverter.convertMultiPartToFile(file);
-		Mat image = Imgcodecs.imread(myFile.getPath());
-		
-		MatOfByte matOfByte = new MatOfByte();
-		Imgcodecs.imencode(".tiff", image, matOfByte);
-		
-		Imgcodecs.imwrite(myFile.getAbsolutePath()+".tiff", image);
-		
-		FileConverter.deleteFile(myFile);
-		try {
-			return ImageIO.read(new ByteArrayInputStream(matOfByte.toArray()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	
-	
-	
-	
+
 }
